ce19092b689ec135b5e6056866938cdf93e5f23a
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-02 13:23:04 +0100

    adding chess coord class

---
+++ src/main/java/dev/negrel/fxchess/Coord.java
@@ -0,0 +1,27 @@
+package dev.negrel.fxchess;
+
+public class Coord {
+	private final int x;
+	private final int y;
+
+	public Coord(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Coord() {
+		this(0, 0);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public String toString() {
+		return "{ " + x + ", " + y + " }";
+	}
+}
