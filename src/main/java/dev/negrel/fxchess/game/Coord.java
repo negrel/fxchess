2121ad6dda91c36b3fe382b0bbbcc860fbb6a85c
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-03 19:39:34 +0100

    adding javadoc to Coord

--- src/main/java/dev/negrel/fxchess/game/Coord.java
+++ src/main/java/dev/negrel/fxchess/game/Coord.java
@@ -1,9 +1,23 @@
 package dev.negrel.fxchess.game;
 
+/**
+ * Coord define a 2D coordinate.
+ */
 public class Coord {
+	/**
+	 * The coordinate on the X axis.
+	 */
 	private final int x;
+	/**
+	 * The coordinate on the Y axis.
+	 */
 	private final int y;
 
+	/**
+	 * Instantiate a new Coord object.
+	 * @param x The coordinate on the X axis.
+	 * @param y The coordinate on the Y axis.
+	 */
 	public Coord(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -13,10 +27,16 @@ public class Coord {
 		this(0, 0);
 	}
 
+	/**
+	 * @return the coordinate on the X axis.
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * @return the coordinate on the Y axis.
+	 */
 	public int getY() {
 		return y;
 	}
