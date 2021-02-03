3275bbb48705df0bc708acee16890d8842c8c31a
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-03 19:56:46 +0100

    renaming Rock to Rook

--- src/main/java/dev/negrel/fxchess/game/piece/Rook.java
+++ src/main/java/dev/negrel/fxchess/game/piece/Rook.java
@@ -7,8 +7,12 @@ import dev.negrel.fxchess.game.Piece;
 import dev.negrel.fxchess.game.board_exception.IllegalPositionException;
 import org.jetbrains.annotations.NotNull;
 
-public class Rock extends Piece {
-	public Rock(@NotNull ChessBoard board, @NotNull Coord coord, @NotNull Color color) throws IllegalPositionException {
+/**
+ * Rook define the Rook chess piece.
+ * @see <a href="https://www.chess.com/terms/chess-pieces#rook">https://www.chess.com/terms/chess-pieces#rook</a>
+ */
+public class Rook extends Piece {
+	public Rook(@NotNull ChessBoard board, @NotNull Coord coord, @NotNull Color color) throws IllegalPositionException {
 		super(board, coord, color);
 	}
 
---
+++ src/test/java/dev/negrel/fxchess/game/piece/RookTest.java
@@ -0,0 +1,36 @@
+package dev.negrel.fxchess.game.piece;
+
+import dev.negrel.fxchess.game.ChessBoard;
+import dev.negrel.fxchess.game.Color;
+import dev.negrel.fxchess.game.Coord;
+import dev.negrel.fxchess.game.board_exception.IllegalPositionException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class RookTest {
+	Rook rook;
+
+	public RookTest() throws IllegalPositionException {
+		rook = new Rook(new ChessBoard(), new Coord(), Color.BLACK);
+	}
+
+	@Test
+	void isValidMove() {
+		assertTrue(rook.isValidMove(new Coord(5, 0)));
+		assertTrue(rook.isValidMove(new Coord(-5, 0)));
+		assertTrue(rook.isValidMove(new Coord(0, 5)));
+		assertTrue(rook.isValidMove(new Coord(0, -5)));
+
+
+		assertFalse(rook.isValidMove(new Coord(1, 5)));
+		assertFalse(rook.isValidMove(new Coord(-1, -1)));
+		assertFalse(rook.isValidMove(new Coord(-1, 5)));
+		assertFalse(rook.isValidMove(new Coord(5, 1)));
+		assertFalse(rook.isValidMove(new Coord(-5, -5)));
+		assertFalse(rook.isValidMove(new Coord(-5, 1)));
+		assertFalse(rook.isValidMove(new Coord(0, 0)));
+	}
+}
