0b49697cbf43df29b3b723a027246aa45d7759e4
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-03 19:59:35 +0100

    add javadoc to exceptions

--- src/main/java/dev/negrel/fxchess/game/board_exception/IllegalPositionException.java
+++ src/main/java/dev/negrel/fxchess/game/board_exception/IllegalPositionException.java
@@ -2,6 +2,9 @@ package dev.negrel.fxchess.game.board_exception;
 
 import dev.negrel.fxchess.game.Coord;
 
+/**
+ * IllegalMoveException are thrown when a coordinate is not between 0 and 8.
+ */
 public class IllegalPositionException extends Exception {
 	public IllegalPositionException(Coord c) {
 		super(c.toString());
