cd07153e05d65559912114fff47019d622d1a230
Alexandre Negrel <negrel.dev@protonmail.com>
2021-03-21 21:36:26 +0100

    fixing tests

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,10 @@ module dev.negrel.fxchess {
 	requires javafx.controls;
 	requires org.jetbrains.annotations;
 
+	opens dev.negrel.fxchess.engine.piece;
+	opens dev.negrel.fxchess.engine;
+
 	exports dev.negrel.fxchess.gui;
+	exports dev.negrel.fxchess.engine.piece;
+	exports dev.negrel.fxchess.engine;
 }
--- src/test/java/dev/negrel/fxchess/engine/piece/PawnTest.java
+++ src/test/java/dev/negrel/fxchess/engine/piece/PawnTest.java
@@ -3,6 +3,7 @@ package dev.negrel.fxchess.engine.piece;
 import dev.negrel.fxchess.engine.ChessBoard;
 import dev.negrel.fxchess.engine.Color;
 import dev.negrel.fxchess.engine.Coord;
+import dev.negrel.fxchess.engine.board_exception.IllegalMoveException;
 import dev.negrel.fxchess.engine.board_exception.IllegalPositionException;
 import org.junit.jupiter.api.Test;
 
@@ -16,22 +17,18 @@ public class PawnTest {
 	void isValidMoveBlackPawn() throws IllegalPositionException {
 		Pawn pawn = new Pawn(new ChessBoard(), new Coord(), Color.BLACK);
 
-		assertTrue(pawn.isValidMove(new Coord(0, 1)));
-		assertTrue(pawn.isValidMove(new Coord(-1, 1)));
-		assertTrue(pawn.isValidMove(new Coord(1, 1)));
+		assertTrue(pawn.isValidMove(new Coord(0, -1)));
 
-		assertFalse(pawn.isValidMove(new Coord(0, -1)));
+		assertFalse(pawn.isValidMove(new Coord(0, 1)));
 	}
 
 	@Test
 	void isValidMoveWhitePawn() throws IllegalPositionException {
 		Pawn pawn = new Pawn(new ChessBoard(), new Coord(), Color.WHITE);
 
-		assertTrue(pawn.isValidMove(new Coord(0, -1)));
-		assertTrue(pawn.isValidMove(new Coord(-1, -1)));
-		assertTrue(pawn.isValidMove(new Coord(1, -1)));
+		assertTrue(pawn.isValidMove(new Coord(0, 1)));
 
-		assertFalse(pawn.isValidMove(new Coord(0, 1)));
+		assertFalse(pawn.isValidMove(new Coord(0, -1)));
 	}
 
 	@Test
@@ -45,10 +42,15 @@ public class PawnTest {
 	}
 
 	@Test
-	void legalMove() throws IllegalPositionException {
+	void legalMove() throws IllegalPositionException, IllegalMoveException {
 		Pawn pawn = new Pawn(new ChessBoard(), new Coord(), Color.WHITE);
 
 		List<Coord> legalMoves = pawn.legalMove();
+		assertEquals(2, legalMoves.size());
+
+		pawn.move(new Coord(pawn.getCoord().getX(), pawn.getCoord().getY() + 1));
+
+		legalMoves = pawn.legalMove();
 		assertEquals(1, legalMoves.size());
 	}
 }
